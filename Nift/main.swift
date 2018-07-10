08189dd577192a5198b0ee4338180b9cd5fcc352
Pablo Sichert <mail@pablosichert.com>
2018-07-10 22:59:20 +0200

    Avoid force unwrapping

--- Nift/main.swift
+++ Nift/main.swift
@@ -109,8 +109,8 @@ func render(_ root: Node) {
 func render(_ children: [Node]?) {
     if let children = children {
         for child in children {
-            for (property, value) in Mirror(reflecting: child.properties).children {
-                print("\(property!): \(value)")
+            for case let (property?, value) in Mirror(reflecting: child.properties).children {
+                print("\(property): \(value)")
             }
 
             child.create(child.properties, child.children).render()
