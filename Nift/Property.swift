c61221ae08569053d4e0512cd1a67e16a1ac422f
Pablo Sichert <mail@pablosichert.com>
2018-08-08 04:43:37 +0200

    Make Property<Root> hashable

--- Nift/Property.swift
+++ Nift/Property.swift
@@ -1,12 +1,21 @@
 import struct Foundation.UUID
 
-private func applier<Root, Value>(path: ReferenceWritableKeyPath<Root, Value>, value: Value) -> (Root) -> Void {
+private func applier<Root, Value>(
+    path: ReferenceWritableKeyPath<Root, Value>,
+    value: Value
+) -> (Root) -> Void {
     return { (_ root: Root) -> Void in
         root[keyPath: path] = value
     }
 }
 
-public struct Property<Root>: Equatable {
+private struct Static {
+    /* Use this value as a fallback, when no sensible hash can be computed.
+     * Should be avoided if possible since sets / hash maps will perform badly. */
+    static let hashValue = UUID().hashValue
+}
+
+public struct Property<Root>: Hashable {
     public static func == (lhs: Property<Root>, rhs: Property<Root>) -> Bool {
         let pathsEqual = lhs.pathsEqual(lhs.path, rhs.path)
         let valuesEqual = lhs.valuesEqual(lhs.value, rhs.value)
@@ -16,6 +25,7 @@ public struct Property<Root>: Equatable {
     }
 
     public let apply: (Root) -> Void
+    public let hashValue: Int
     public let id = UUID()
     public let path: Any
     public let pathsEqual: (Any, Any) -> Bool
@@ -25,10 +35,25 @@ public struct Property<Root>: Equatable {
     public init<Value>(
         _ path: ReferenceWritableKeyPath<Root, Value>,
         _ value: Value
+    ) where Value: Hashable {
+        typealias Path = ReferenceWritableKeyPath<Root, Value>
+
+        self.apply = applier(path: path, value: value)
+        self.hashValue = value.hashValue
+        self.path = path
+        self.pathsEqual = Equal<Path>.call
+        self.value = value
+        self.valuesEqual = Equal<Value>.call
+    }
+
+    public init<Value>(
+        _ path: ReferenceWritableKeyPath<Root, Value>,
+        _ value: Value
     ) where Value: Equatable {
         typealias Path = ReferenceWritableKeyPath<Root, Value>
 
         self.apply = applier(path: path, value: value)
+        self.hashValue = Static.hashValue
         self.path = path
         self.pathsEqual = Equal<Path>.call
         self.value = value
@@ -42,6 +67,7 @@ public struct Property<Root>: Equatable {
         typealias Path = ReferenceWritableKeyPath<Root, Value>
 
         self.apply = applier(path: path, value: value)
+        self.hashValue = id.hashValue
         self.path = path
         self.pathsEqual = Equal<Path>.call
         self.value = value
