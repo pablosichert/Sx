515fac99c9d558c33f8e1e77deea67413ececdaf
Pablo Sichert <mail@pablosichert.com>
2018-08-09 21:44:14 +0200

    Remove operator on ReferenceWritableKeyPath due to unsatisfying results

--- Nift/ReferenceWritableKeyPath+-.swift
+++
@@ -1,8 +0,0 @@
-public extension ReferenceWritableKeyPath {
-    public static func - <Root, Value>(
-        lhs: ReferenceWritableKeyPath<Root, Value>,
-        rhs: Value
-    ) -> Property<Root> {
-        return Property<Root>(lhs, rhs)
-    }
-}
