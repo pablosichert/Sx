8683df5dc19900980806612d43b603ecaaabce56
Pablo Sichert <mail@pablosichert.com>
2018-08-09 21:46:47 +0200

    Share logic in Property<Root> by common constructor

--- Nift/Property.swift
+++ Nift/Property.swift
@@ -1,17 +1,8 @@
 import struct Foundation.UUID
 
-private func applier<Root, Value>(
-    path: ReferenceWritableKeyPath<Root, Value>,
-    value: Value
-) -> (Root) -> Void {
-    return { (_ root: Root) -> Void in
-        root[keyPath: path] = value
-    }
-}
-
 private struct Static {
     /* Use this value as a fallback, when no sensible hash can be computed.
-     * Should be avoided if possible since sets / hash maps will perform badly. */
+     * Should be avoided if possible since hash sets / maps will perform badly. */
     static let hashValue = UUID().hashValue
 }
 
@@ -26,51 +17,66 @@ public struct Property<Root>: Hashable {
 
     public let apply: (Root) -> Void
     public let hashValue: Int
-    public let id = UUID()
+    public let id: UUID
     public let path: Any
     public let pathsEqual: (Any, Any) -> Bool
     public let value: Any
     public let valuesEqual: (Any, Any) -> Bool
 
+    init<Value>(
+        hashValue: Int,
+        id: UUID = UUID(),
+        path: ReferenceWritableKeyPath<Root, Value>,
+        value: Value,
+        valuesEqual: @escaping (Any, Any) -> Bool
+    ) {
+        typealias Path = ReferenceWritableKeyPath<Root, Value>
+
+        self.apply = { root in root[keyPath: path] = value }
+        self.hashValue = hashValue
+        self.id = id
+        self.path = path
+        self.pathsEqual = Equal<Path>.call
+        self.value = value
+        self.valuesEqual = valuesEqual
+    }
+
     public init<Value>(
         _ path: ReferenceWritableKeyPath<Root, Value>,
         _ value: Value
     ) where Value: Hashable {
-        typealias Path = ReferenceWritableKeyPath<Root, Value>
-
-        self.apply = applier(path: path, value: value)
-        self.hashValue = value.hashValue
-        self.path = path
-        self.pathsEqual = Equal<Path>.call
-        self.value = value
-        self.valuesEqual = Equal<Value>.call
+        self.init(
+            hashValue: value.hashValue,
+            path: path,
+            value: value,
+            valuesEqual: Equal<Value>.call
+        )
     }
 
     public init<Value>(
         _ path: ReferenceWritableKeyPath<Root, Value>,
         _ value: Value
     ) where Value: Equatable {
-        typealias Path = ReferenceWritableKeyPath<Root, Value>
-
-        self.apply = applier(path: path, value: value)
-        self.hashValue = Static.hashValue
-        self.path = path
-        self.pathsEqual = Equal<Path>.call
-        self.value = value
-        self.valuesEqual = Equal<Value>.call
+        self.init(
+            hashValue: Static.hashValue,
+            path: path,
+            value: value,
+            valuesEqual: Equal<Value>.call
+        )
     }
 
     public init<Value>(
         _ path: ReferenceWritableKeyPath<Root, Value>,
         _ value: Value
     ) {
-        typealias Path = ReferenceWritableKeyPath<Root, Value>
+        let id = UUID()
 
-        self.apply = applier(path: path, value: value)
-        self.hashValue = id.hashValue
-        self.path = path
-        self.pathsEqual = Equal<Path>.call
-        self.value = value
-        self.valuesEqual = { (_: Any, _: Any) -> Bool in false }
+        self.init(
+            hashValue: id.hashValue,
+            id: id,
+            path: path,
+            value: value,
+            valuesEqual: { (_: Any, _: Any) -> Bool in false }
+        )
     }
 }
