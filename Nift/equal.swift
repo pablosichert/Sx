5b7630a3d6373eea3367ede364acd44c978cc972
Pablo Sichert <mail@pablosichert.com>
2018-08-24 08:13:21 +0200

    Implement equal function to check if the addresses of two functions are the same

---
+++ Nift/equal.swift
@@ -0,0 +1,31 @@
+/*
+ Based on the reverse engineering efforts of Dmitry Rodionov
+ https://github.com/rodionovd/SWRoute/wiki/Function-hooking-in-Swift
+ */
+private struct FunctionWrapper {
+    let unknown0: UnsafePointer<Any>
+    let function: UnsafePointer<Function>
+}
+
+private struct Function: Equatable {
+    let type: UnsafePointer<Any>
+    let unknown0: UnsafePointer<Any>
+    let address: UnsafePointer<Any>
+    let context: UnsafePointer<Any>
+
+    static func from<Arguments, Return>(
+        _ function: (Arguments) -> Return
+    ) -> Function {
+        return unsafeBitCast(function, to: FunctionWrapper.self).function.pointee
+    }
+}
+
+public func equal<Arguments, Return>(
+    lhs: (Arguments) -> Return,
+    rhs: (Arguments) -> Return
+) -> Bool {
+    let lhs = Function.from(lhs)
+    let rhs = Function.from(rhs)
+
+    return lhs.address == rhs.address
+}
